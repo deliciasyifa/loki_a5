0ba7c1640b272ceab6dd4a08e888bf5a90e95606
deliciasyifa <2011522027_delicia@student.unand.ac.id>
2022-03-18 00:17:39 +0700

    route untuk halaman logout

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const express = require ('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const x = express();
-const port = 8000
+const port = 8000;
 
 //route awal
-x.get('/',(req, res) => {
-    res.send('Welcome di Server');
+x.get("/", (req, res) => {
+  res.send("Welcome di Server");
 });
 
 //route untuk halaman login
-x.get('/login',(req, res) => {
-  res.send('Halaman Login');
+x.get("/login", (req, res) => {
+  res.send("Halaman Login");
 });
 
-x.listen(port, () =>{
-  console.log(`Server berada pada port ${port}`)
-})
+//route untuk halaman logout
+x.get("/logout", (req, res) => {
+  res.send("Ini merupakan halaman logout :)");
+});
+
+x.listen(port, () => {
+  console.log(`Server berada pada port ${port}`);
+});
