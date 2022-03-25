a4b8f20a995265da77d23691fb19e9fe39c556cc
daffamuliya <daffamuliya15@gmail.com>
2022-03-25 09:56:37 +0700

    menambahkan fungsional 7

--- RPS.js
+++ RPS.js
@@ -16,4 +16,20 @@ router.post("/addrps", (req, res) => {
   console.log("RPS berhasil ditambahkan");
 });
 
+//route mengubah RPS yang sudah ada (fungsional 7)
+router.post("/changerps", (req, res) => {
+  let add = {
+    message: "Silahkan tambahkan RPS disini",
+    "mata kuliah": {
+      "kode matkul": "JSI62126",
+      "nama matkul": "Pemrograman Web",
+      "jumlah sks": "2",
+      semester: "4",
+    },
+  };
+  res.json(add);
+  console.log("RPS berhasil diubah");
+});
+
+
 module.exports = router;
