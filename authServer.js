fb71cabbf9001370ba203ff2a8957c8200871a87
Dina1003 <dinalatief.padanglua@gmail.com>
2022-06-18 06:56:29 +0700

    dina jwt

---
+++ authServer.js
@@ -0,0 +1,59 @@
+require('dotenv').config()
+
+const express = require("express");
+const x = express();
+const jwt = require('jsonwebtoken')
+
+const cpmk = require('./cpmk');
+const conn = require('./config/conn');
+const komponen_nilai = require('./komponen_nilai');
+const pertemuan_mingguan = require('./pertemuan_mingguan');
+const referensi = require('./referensi');
+const RPS = require('./RPS');
+const connect_seque = require('./CPMK_ORM/connect_seque');
+const course_los = require('./CPMK_ORM/course_los');
+
+const port = 3000;
+
+//tes jwt
+x.use(express.json())
+
+let refreshTokens = []
+
+x.post('/token', (req, res)=>{
+  const refreshToken = req.body.token
+  if(refreshToken == null) return res.sendStatus(401)
+  if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user)=>
+  {
+    if(err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ name: user.name})
+    res.json({accessToken: accessToken})
+  })
+})
+
+x.post('/login', (req, res)=>{
+  //authenticate user
+  const username = req.body.username
+  const user = {name: username}
+
+  const accessToken = generateAccessToken(user)
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+  refreshTokens.push(refreshToken)
+  res.json({accessToken: accessToken, refreshToken: refreshToken })
+})
+
+function generateAccessToken(user){
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'30s'})
+}
+
+
+
+
+x.use("/public", express.static("public"));
+
+
+
+x.listen(port, () => {
+  console.log(`Server berada pada port ${port}`);
+});
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+
 const express = require("express");
 const x = express();
+const jwt = require('jsonwebtoken')
 
 const cpmk = require('./cpmk');
 const conn = require('./config/conn');
@@ -12,15 +15,45 @@ const course_los = require('./CPMK_ORM/course_los');
 
 const port = 8000;
 
+//jwt
+x.use(express.json())
+const posts=[
+  {
+    username: 'dina',
+    title: 'loki1'
+  },
+  {
+    username: 'riska',
+    title: 'loki2'
+  }
+]
 
+x.get('/posts', authenticateToken,  (req, res)=> {
+  res.json(posts.filter(post=> post.username === req.user.name))
+})
+
+function authenticateToken(req, res, next){
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if(token == null) return res.sendStatus(401)
+ 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user )=>
+  {
+    if(err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
+
+//
 
 //route awal
-x.get("/", (req, res) => {
-  res.send("Welcome di Server JS");
+x.get("/",  (req, res) => {
+  res.send("Welcome di Server JS")
 });
 
 //route untuk halaman login (fungsional 1)
-x.get("/login", (req, res) => {
+x.post("/login", (req, res) => {
   res.send("Ini adalah Halaman Login");
 });
 
@@ -31,12 +64,14 @@ x.get("/logout", (req, res) => {
 
 x.use("/bagian", referensi);
 x.use("/bagian", komponen_nilai);
+x.use("/bagian", pertemuan_mingguan);
 x.use("/bagian", RPS);
 x.use("/bagian", cpmk);
-x.use("/bagian", pertemuan_mingguan);
 x.use("/bagian", conn);
 x.use("/bagian", connect_seque);
 x.use("/bagian", course_los);
+
+
 
 x.use("/public", express.static("public"));
 
