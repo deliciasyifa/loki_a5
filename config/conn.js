f45fac11e763c6b1121be15810d1083f072dd2dc
deliciasyifa <2011522027_delicia@student.unand.ac.id>
2022-06-20 12:31:52 +0700

    .

--- config/conn.js
+++ config/conn.js
@@ -1,39 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const mysql = require('mysql2'); 
+const mysql = require("mysql2");
 
-router.get('/database', function(req, res, next){
+router.get("/database", function (req, res, next) {
+  const mysql = require("mysql");
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "loki",
+    password: "",
+  });
 
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password:"",
-        database:"loki"
-    
-    });
+  connection.connect();
+  try {
+    console.log("Koneksi berhasil");
+  } catch (error) {
+    console.error("Tidak dapat koneksi ke database:", error);
+  }
 
-    connection.connect();
-    try {
-        
-        console.log('Koneksi berhasil');
-      } catch (error) {
-        console.error('Tidak dapat koneksi ke database:', error);
-      }
+  // coba query
+  let sql = "SELECT * FROM users";
+  connection.query(sql, function (err, result, fields) {
+    if (err) throw err;
 
-    // coba query
-    let sql = "SELECT * FROM users";
-    connection.query(sql, function(err, result, fields){
-        if(err) throw err;
-
-        res.json(result);
-    });
-
-    
-
+    res.json(result);
+  });
 });
 
 // console.log("Berhasil Connect")
-
-
 
 module.exports = router;
