3dbefb9903825c62f8792ccee993bf38804c2983
deliciasyifa <2011522027_delicia@student.unand.ac.id>
2022-06-20 16:45:18 +0700

    landing page database oke

---
+++ models/indexModel.js
@@ -0,0 +1,18 @@
+module.exports = {
+  fetchData: (db, callback) => {
+    db.query("SELECT * FROM course_plans", callback);
+  },
+
+  getById: (db, id, callback) => {
+    db.query("SELECT * FROM course_plans WHERE id = ", id, callback);
+  },
+  insertData: (db, data, callback) => {
+    db.query("INSERT INTO course_plans SET ?", data, callback);
+  },
+  updateData: (db, id, data, callback) => {
+    db.query("UPDATE course_plans SET ? WHERE id = ", id, data, callback);
+  },
+  deleteData: (db, id, callback) => {
+    db.query("DELETE FROM course_plans WHERE id = ", id, callback);
+  },
+};
