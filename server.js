fc957375560c548f7212c6fd84c0630273f300e9
Delicia Syifa Maghfira <88974186+deliciasyifa@users.noreply.github.com>
2022-07-05 18:47:12 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const port = 2000;
+const controller = require(`./controllers/indexcontroller`);
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const { isDosen, isAdmin, checkUser } = require(`./middleware/authToken`);
+dotenv.config();
+
+const { print } = require("pdf-to-printer");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("*", checkUser);
+
+//router
+const mahasiswa = require("./Router/mahasiswa");
+app.use("/mahasiswa", mahasiswa);
+
+const dosen = require("./Router/dosen");
+app.use("/dosen", isDosen, dosen);
+
+const admin = require("./Router/admin");
+app.use("/admin", isAdmin, admin);
+
+const auth = require("./Router/auth");
+app.use("/auth", auth);
+
+//--------------------------------
+
+app.get("/", (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.redirect("/mahasiswa/rps");
+  } else {
+    res.render("index", { dasbordaktif: "active", rpsaktif: "" });
+  }
+});
+
+//lihat daftar user
+app.get("/user", controller.users.retrieveAll);
+
+app.get("/profil", (req, res) => {
+  const token = req.cookies.token;
+  if (!token) return res.redirect("/auth/login");
+
+  res.render("profil", { dasbordaktif: "", rpsaktif: "" });
+});
+
+app.get("/print", (req, res) => {
+  res.render("RPS");
+  // print("views/RPS.ejs");
+});
+
+//----------------------------------
+app.use("/", (req, res) => {
+  res.render("eror404");
+});
+
+app.listen(port, () => {
+  console.log(`Server Sedang Berjalan di http://localhost:${port}`);
+});
